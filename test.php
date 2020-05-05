de0c7e2c0dd40b93726c8ab0d844a812b7f6db1b
Your Name <you@example.com>
2020-05-04 21:00:36 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,34 +1,65 @@
 <?php
 $TheBoard=array(9);
 $isWon = false;
+$winner = "";
 
 //Initializing the board
 for ($i=0;$i<9;$i++) $TheBoard[$i]='_';
 
 printf("Welcome to TIC-TAC-TOE EXTREME Lite\r\n");
 
-print_board();
+printf("\r\n");
+printf ("Would you like to go first? (y/n)");
 
-playerMove();
-
-botMove();
-
-print_board();
-
-playerMove();
-
-botMove();
+$whoFirst = readline();
 
 print_board();
 
-playerMove();
+while(!$isWon) {
+  if ($whoFirst == "y") {
+    playerMove();
+  }
+  else {
+    botMove();
+  }
+  
+  print_board();
 
-print_board();
-printf("\r\nWINNER!!");
+  checkWin();
+  if($isWon) {
+    winMessage();
+  }
+  else {
+    if ($whoFirst == "y") {
+      botMove();
+    }
+    else {
+      playerMove();
+    }
+    print_board();
+    checkWin();
+    if($isWon) {
+      winMessage();
+    }
+  }
+}
+
+function winMessage() {
+  global $winner;
+
+  printf("\r\n");
+  if($winner = "X") {
+    printf ("Player Wins!");
+  } else {
+    printf ("Bot Wins!");
+  }
+  printf("\r\n");
+}
 
 function playerMove() {
   global $TheBoard;
 
+  printf("\r\n");
   printf ("Player Turn...\r\n");
 
   $step = intval(readline());
@@ -43,26 +74,50 @@ function playerMove() {
 function botMove() {
   global $TheBoard;
   $done = false;
+
+  printf("\r\n");
+  printf ("Bot Turn...\r\n");
+
   while (!$done) {
     $r = rand(0,8);
-    if ($TheBoard[$r] == "_")
+    if ($TheBoard[$r] == "_") {
       $TheBoard[$r] = 'O';
       $done = true;
-  } 
+    }
+  }
+
+  printf ($r);
+  printf("\r\n");
+}
+
+function checkWin() {
+  global $isWon;
+  global $winner;
+  global $TheBoard;
+
+  $winConditions = array("012", "345", "678", "036", "147", "258", "048", "246");
+  
+  foreach ($winConditions as $value) {
+    $split = str_split($value);
+    $values = array($TheBoard[$split[0]], $TheBoard[$split[1]], $TheBoard[$split[2]]);
+    if(($values[0] != "_") && ($values[0] == $values[1]) && ($values[2] == $values[0])) {
+      $isWon = true;
+      $winner = $values[2];
+    }
+  }
 }
 
 function print_board()
 {
   global $TheBoard;
-  global $isWon;
 
+  printf("\r\n");
   printf("%s %s %s", $TheBoard[0], $TheBoard[1], $TheBoard[2]);
   printf("\r\n");
   printf("%s %s %s", $TheBoard[3], $TheBoard[4], $TheBoard[5]);
   printf("\r\n");
   printf("%s %s %s", $TheBoard[6], $TheBoard[7], $TheBoard[8]);
   printf("\r\n");
-  printf($isWon);
 }
                         
 ?>
